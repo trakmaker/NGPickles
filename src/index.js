c524006deff9148bac2724cfec5763bf129bf18b
KAKKIRENI RAVI TEJA <ravitejak966@gmail.com>
2025-03-11 23:26:09 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import App from "./App";
+import { BrowserRouter as Router } from "react-router-dom";
 import "./index.css";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <Router>
+    <App />
+  </Router>
+);
